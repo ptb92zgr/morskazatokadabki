8af021325e6c95694855db5061835ab3a9ece0ce
Patryk Janicki <patrykjanicki@MacBook-Air-Patryk.local>
2023-02-04 15:08:38 +0100

    contact form to do and mediaquaries

--- index.js
+++ index.js
@@ -2,54 +2,39 @@ const toggleMenuBtn = document.querySelector(".menu__btn");
 const menuItems = document.querySelector(".menu__items");
 const menuLink = document.querySelectorAll("menu__link");
 const lines = document.querySelectorAll("path");
-//automatic slider for about section
+const sections = document.querySelectorAll(".section");
+let slideIndex = 1;
 
-// automaticSlides();
-// function automaticSlides() {
-//   let i;
-//   let autSlides = document.getElementsByClassName("section__slider-item");
-//   for (i = 0; i < autSlides.length; i++) {
-//     autSlides[i].style.display = "none";
-//   }
-//   automaticSlideIndex++;
-//   if (automaticSlideIndex > autSlides.length) {
-//     automaticSlideIndex = 1;
-//   }
+// //slider
+showSlides(slideIndex);
 
-//   autSlides[automaticSlideIndex - 1].style.display = "block";
-//   setTimeout(automaticSlides, 2000);
-// }
+// Button controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-// //slider for beers
-// showSlides(slideIndex);
+// slide remote
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-// // Button controls
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("section__slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
 
-// // slide remote
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("section__slide");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-
-//     if (window.matchMedia("(min-width: 1024px)")) {
-//     }
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-// }
+    if (window.matchMedia("(min-width: 1024px)")) {
+    }
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
 
 const showMenu = () => {
   menuItems.classList.toggle("show__menu");
